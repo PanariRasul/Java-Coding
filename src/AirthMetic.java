0347475420908598b1c7a2959045636a246a7c4d
PanariRasul <panarirasul@gmail.com>
2023-12-01 19:19:34 +0530

    Aithmatic Problems

---
+++ src/AirthMetic.java
@@ -0,0 +1,29 @@
+public class AirthMetic {
+
+    public static void main(String[] args) {
+        float a = 5;
+        float b = 9;
+        float c = 2;
+        float v = 7;
+        float u = 4;
+        float s = 6;
+
+        float ans1 =7/4*9/2;
+        System.out.println("First possible Answer: "+ ans1);
+
+        float ans2 = 7/4f * 9/2f;
+        System.out.println("Second possible Answer: "+ans2);
+
+        //second problem
+
+        float result = (v*v-u*u)/(2*a*s);
+        System.out.println(result);
+
+
+        //Faind Value Of X
+        int x = 7;
+        float t = 7.49f / 7 +35f /7f ;
+        System.out.println(t);
+
+    }
+}
